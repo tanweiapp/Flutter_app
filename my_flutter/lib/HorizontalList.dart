925b873898314a5ae1e6d9c03bc4cc85c6775a6e
Smile <452567933@qq.com>
2018-11-28 18:22:13 +0800

    commit

--- my_flutter/lib/HorizontalList.dart
+++ my_flutter/lib/HorizontalList.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
+class MyHorizontalList extends StatelessWidget{
+  Widget build(BuildContext context){
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+           width:180.0,
+            color: Colors.lightGreenAccent,
+        ),
+        new Container(
+            child: new Image.network(
+              'http://jspang.com/static/myimg/smile-vue.jpg',
+              fit: BoxFit.contain,
+            ),
+            width: 180,
+            color: Colors.lightBlue,
+          ),
+        new Container(
+            width: 180,
+            color: Colors.greenAccent,
+          ),
+        new Container(
+            width: 180,
+            color: Colors.deepPurpleAccent,
+          ),
+        new Container(
+            width: 180,
+            color: Colors.deepOrangeAccent,
+          ),
+      ],
+    );
+  }
+}
 class HorizontalList extends StatelessWidget
 {
   Widget build(BuildContext context){
@@ -12,34 +45,50 @@ class HorizontalList extends StatelessWidget
         body: Center(
           child: Container(
             height: 200,
-            child: new ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                new Container(
-                  child: new Image.network(
-                    'http://jspang.com/static/myimg/smile-vue.jpg',
-                    fit: BoxFit.contain,
-                  ),
-                  width: 180,
-                  color: Colors.lightBlue,
-                ),
-                new Container(
-                  width: 180,
-                  color: Colors.greenAccent,
-                ),
-                new Container(
-                  width: 180,
-                  color: Colors.deepPurpleAccent,
-                ),
-                new Container(
-                  width: 180,
-                  color: Colors.deepOrangeAccent,
-                ),
-              ],
-            ),
+            child: MyHorizontalList(),
           ),
         ),
       ),
     );
   }
+  // Widget build(BuildContext context){
+  //   return MaterialApp(
+  //     title: 'Horizontal List',
+  //     home: Scaffold(
+  //       appBar: new AppBar(
+  //         title: new Text('Horizontal List'),
+  //       ),
+  //       body: Center(
+  //         child: Container(
+  //           height: 200,
+  //           child: new ListView(
+  //             scrollDirection: Axis.horizontal,
+  //             children: <Widget>[
+  //               new Container(
+  //                 child: new Image.network(
+  //                   'http://jspang.com/static/myimg/smile-vue.jpg',
+  //                   fit: BoxFit.contain,
+  //                 ),
+  //                 width: 180,
+  //                 color: Colors.lightBlue,
+  //               ),
+  //               new Container(
+  //                 width: 180,
+  //                 color: Colors.greenAccent,
+  //               ),
+  //               new Container(
+  //                 width: 180,
+  //                 color: Colors.deepPurpleAccent,
+  //               ),
+  //               new Container(
+  //                 width: 180,
+  //                 color: Colors.deepOrangeAccent,
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
