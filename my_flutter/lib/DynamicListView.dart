063c63b2a37cd1ea132fce8e02b0a54389ee269a
Smile <452567933@qq.com>
2018-12-07 09:43:34 +0800

    message

---
+++ my_flutter/lib/DynamicListView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DynamicListView extends StatelessWidget{
+
+  final List<String> items;
+  // final List<String> items;
+  DynamicListView({Key key,@required this.items}):super(key:key);
+  // DynamicListView({Key key, @required this.items}):super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Dynamic ListView',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dynamic ListView'),
+        ),
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context,index){
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ my_flutter/lib/TitleSection.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+class TitleSection extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(top:10,bottom: 20),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                     'Oeschinen Lake Campgroundampground',
+                    style:new TextStyle(
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                ),
+                new Text(
+                   'Kandersteg,Switzerland',
+                  style:new TextStyle(
+                    color:Colors.grey
+                  )
+                )
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color:Colors.red
+          ),
+          new Text('41'),
+          
+        ],
+      ),
+    );
+  }
+}
